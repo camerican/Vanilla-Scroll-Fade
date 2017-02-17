2a4068a82ec7cdbf2e6caf784e8f9bca65e96b46
Cam Crews <camcrews@gmail.com>
2017-02-17 15:45:16 -0500

    handleScroll function

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 document.addEventListener("DOMContentLoaded",function(){
-	window.addEventListener("scroll",function(){
-		var windowBottom = this.pageYOffset + this.innerHeight;
-		var elements = document.querySelectorAll(".fade");
-		for( var i=0; i<elements.length; i++) {
-			let elCurrent = elements[i];
-			let objectBottom = elCurrent.offsetTop + elCurrent.offsetHeight;
-			if( objectBottom < windowBottom ) {
-				if( elCurrent.style.opacity == 0 ) {
-					elCurrent.style.opacity = 1;
-				}
-			} else {
-				if( elCurrent.style.opacity == 1 ) {
-        	elCurrent.style.opacity = 0;
-				}
+	handleScroll();	// trigger handling for first time
+	window.addEventListener("scroll",handleScroll);
+	// and everytime we scroll thereafter
+});
+
+function handleScroll(){
+	var windowBottom = window.pageYOffset + window.innerHeight;
+	var elements = document.querySelectorAll(".fade");
+	for( var i=0; i<elements.length; i++) {
+		let elCurrent = elements[i];
+		let objectBottom = elCurrent.offsetTop + elCurrent.offsetHeight;
+		if( objectBottom < windowBottom ) {
+			if( elCurrent.style.opacity == 0 ) {
+				elCurrent.style.opacity = 1;
+			}
+		} else {
+			if( elCurrent.style.opacity == 1 ) {
+      	elCurrent.style.opacity = 0;
 			}
 		}
-	});
-	// trigger scroll to top of the page on load
-	document.body.scrollTop = 0;
-});
+	}
+}
